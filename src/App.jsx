68def406843f9c35498df2cfca71192aded6134e
fi0321 <fi0321@mun.ca>
2024-05-06 15:57:27 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import HomePage from './pages/HomePage';
+import GamePage from './pages/GamePage';
+// Import other pages here
+
+function App() {
+  return (
+    <Router>
+      <div>
+        {/* Place any global components like a navigation bar here */}
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/game' element={<GamePage />} />
+          {/* Define more routes as needed */}
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/Components/OverLay.jsx
@@ -0,0 +1,51 @@
+import { TiArrowSortedUp } from 'react-icons/ti';
+import { TiArrowSortedDown } from 'react-icons/ti';
+
+const OverLay = ({ Needcity2, Needcity, changeCity, showTemperature2 }) => {
+  return (
+    <div className='absolute flex  justify-center items-center h-screen w-full '>
+      <div className='flex flex-col m-5 '>
+        {!showTemperature2 && (
+          <>
+            <p className='text-center text-yellow-200 text-4xl '>
+              Does{' '}
+              <span className='italic font-serif text-4xl font-semibold'>
+                {Needcity2}
+              </span>{' '}
+              have a
+            </p>{' '}
+            <div className='flex flex-col items-center w-auto'>
+              <button
+                className='group rounded-full  bg-amber-400/40 hover:hover:bg-amber-600/60 hover:border-white text-yellow text-5xl px-5 py-3 m-5 border-2'
+                onClick={() => changeCity(true)}
+              >
+                <div className='px-5 pt-3  flex justify-center group-hover:motion-reduce:animate-bounce'>
+                  <TiArrowSortedUp className='text-white'></TiArrowSortedUp>
+                </div>
+                <div className='px-5 py-2 text-white'>Higher</div>
+              </button>
+
+              <button
+                className='group rounded-full bg-amber-400/40 hover:bg-amber-600/60 m-5 hover:border-white text-yellow text-5xl px-5 py-3 border-2'
+                onClick={() => changeCity(false)}
+              >
+                <div className='px-5 py-2 text-white'>Lower</div>
+                <div className='px-5 pt-3  flex justify-center group-hover:motion-reduce:animate-bounce'>
+                  <TiArrowSortedDown className='	text-white			'></TiArrowSortedDown>
+                </div>
+              </button>
+              <p className='text-center text-yellow-200 text-4xl '>
+                temperature than{' '}
+                <span className='italic font-serif text-4xl font-semibold'>
+                  {Needcity}
+                </span>
+                ?
+              </p>
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+export default OverLay;
